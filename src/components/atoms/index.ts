741853970e2553a3aca4e1d86024212c2c5b8d0f
RivierGrullon <grullonrivier@gmail.com>
2023-07-18 11:37:03 -0400

    feat: Add classnames package and export additional components

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,8 @@
+import * as Heading from './base-text/heading';
+import * as Body from './body';
+import Button from './button';
+export * from './help-text';
+export * from './input';
+export * from './label';
+
+export { Heading, Button, Body };
