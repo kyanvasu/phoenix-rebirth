3df16c692da76be3b23d348b9c3a58591a67a54d
RivierGrullon <grullonrivier@gmail.com>
2023-07-19 13:36:09 -0400

    refactor(input): remove unused input component

--- src/components/molecules/form/text-input/index.tsx
+++ src/components/molecules/form/text-input/index.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import React from 'react';
-import { HelpText, Input, Label } from '../../../atoms';
+import { HelpText, Label } from '../../../atoms';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -25,7 +25,15 @@ export function TextInput({
     >
       {label && <Label required={required}>{label}</Label>}
       <div className='relative shadow-sm'>
-        <Input error={error} {...others} />
+        <input
+          className={classNames(
+            'block w-full h-9 rounded-md border border-base-neutral-grey-30 p-2 text-base-neutral-grey-100 placeholder:text-base-neutral-grey-70 focus:outline-none focus:border-base-primary-80 text-body-md disabled:bg-base-neutral-grey-30',
+            {
+              'border-red-500': error,
+            }
+          )}
+          {...others}
+        />
       </div>
 
       {helpText && <HelpText error={error}>{helpText}</HelpText>}
