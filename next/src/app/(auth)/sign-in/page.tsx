4513cbe7829404871c12fb55e6ece5255b228ccb
RivierGrullon <grullonrivier@gmail.com>
2023-07-24 18:14:38 -0400

    refactor: Remove unused imports and code from sign-in and sign-up pages

--- next/src/app/(auth)/sign-in/page.tsx
+++ next/src/app/(auth)/sign-in/page.tsx
@@ -1,87 +1,9 @@
-'use client'
-import Link from 'next/link';
+'use client';
 import React from 'react';
-import { useState } from 'react';
-import { Atoms, Molecules } from '@kanvas/phoenix';
-// TODO: add translates
-function useSignIn() {
-  const [checkboxState, setCheckboxState] = useState(true);
-
-  function handleToggle() {
-    setCheckboxState(!checkboxState);
-  }
-
-  return {
-    models: {
-      checkboxState,
-    },
-    operations: {
-      handleToggle,
-    },
-  };
-}
+import { Auth } from '@kanvas/phoenix/client';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
-  const { models, operations } = useSignIn();
-  return (
-    <div>
-      <Atoms.Heading.Five className='mt-6 mb-10 font-bold text-base-neutal-grey-100'>
-        Sign in to your account
-      </Atoms.Heading.Five>
-      <form className='flex flex-col gap-y-[38px]'>
-        <Molecules.Form.TextInput
-          type='email'
-          label={'email'}
-          placeholder={'email'}
-          name='email'
-        />
-        <Molecules.Form.TextInput
-          type='password'
-          name='password'
-          label='password'
-          placeholder='password'
-          required
-        />
-
-        <div>
-          <div className='flex flex-row justify-between'>
-            <Molecules.Form.CheckboxInput
-              id='remember-me'
-              label={'remember me'}
-              checked={models.checkboxState}
-              onChange={operations.handleToggle}
-            />
-
-            <Link
-              className='font-semibold text-base-primary-100 text-body-md'
-              href='/forgot-password'
-            >
-              Forgot Password
-            </Link>
-          </div>
-
-          <Atoms.Button.Solid
-            className='justify-center w-full my-6'
-            size='small'
-            type='submit'
-          >
-            Sign In
-          </Atoms.Button.Solid>
-
-          <span className='flex flex-row gap-x-4'>
-            <Atoms.Body.Three className='text-base-neutral-grey-80'>
-              Don't Have an account?
-            </Atoms.Body.Three>
-
-            <Link
-              className='font-semibold text-base-primary-100 text-body-md'
-              href='/sign-up'
-            >
-              Register
-            </Link>
-          </span>
-        </div>
-      </form>
-    </div>
-  );
+  const router = useRouter();
+  return <Auth.LoginPage redirect={() => router.push('/home')} />;
 }
--- src/components/templates/auth-layout/index.tsx
+++ src/components/templates/auth-layout/index.tsx
@@ -5,18 +5,16 @@ interface Props extends React.PropsWithChildren {
 }
 export default function AuthLayout({ children, backgroundImage }: Props) {
   return (
-    <html lang='en'>
-      <body className='flex flex-col md:flex-row'>
-        <aside className='relative w-full md:w-1/2 h-[50vh] md:h-screen'>
-          {backgroundImage}
-        </aside>
+    <>
+      <aside className='relative w-full md:w-1/2 h-[50vh] md:h-screen'>
+        {backgroundImage}
+      </aside>
 
-        <main className='flex flex-col items-center justify-center w-full md:w-1/2'>
-          <article className='w-[400px] flex flex-col rounded-md p-6 border border-base-neutral-grey-40'>
-            {children}
-          </article>
-        </main>
-      </body>
-    </html>
+      <main className='flex flex-col items-center justify-center w-full md:w-1/2'>
+        <article className='w-[400px] flex flex-col rounded-md p-6 border border-base-neutral-grey-40'>
+          {children}
+        </article>
+      </main>
+    </>
   );
 }
