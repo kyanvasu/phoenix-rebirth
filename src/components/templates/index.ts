9100941e970436ff59d80f473adc445ca1288d0a
RivierGrullon <grullonrivier@gmail.com>
2023-08-11 15:08:23 -0400

    feat: Add LeadsLayout component

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,7 +1,8 @@
 import BaseLayout from "./base-layout";
 import AuthLayout from "./auth-layout";
-
+import LeadsLayout from "./leads-layout";
 export {
   BaseLayout,
-  AuthLayout
+  AuthLayout,
+  LeadsLayout
 }
