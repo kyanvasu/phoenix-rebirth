fc014fcc98a244b683399ad95ec838aa0208d9a5
RivierGrullon <grullonrivier@gmail.com>
2023-08-24 14:20:22 -0400

    refactor(table): refactor table component and remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,5 @@ import * as Organism from './components/organism';
 import * as Atoms from './components/atoms';
 import * as Molecules from './components/molecules';
 import * as Templates from './components/templates';
-
 export { Organism, Atoms, Molecules, Templates };
-
 export * from "./pages"
