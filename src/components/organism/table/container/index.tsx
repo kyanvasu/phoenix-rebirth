1c837023fd7f4e5dfdd8c9de64e037c143c4115b
RivierGrullon <grullonrivier@gmail.com>
2023-09-21 17:29:38 -0400

    style: Remove unnecessary class names from table container

--- src/components/organism/table/container/index.tsx
+++ src/components/organism/table/container/index.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export function TContainer({ children }: { children: React.ReactNode }) {
   return (
     <div className='flow-root'>
-      <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+      <div className='-mx-4 -my-2 sm:-mx-6 lg:-mx-8'>
         <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
-          <div className='overflow-hidden ring-1 shadow-elevation-1 ring-black ring-opacity-5 sm:rounded-lg'>
+          <div className='ring-1 shadow-elevation-1 ring-black ring-opacity-5 sm:rounded-lg'>
             <table className='min-w-full divide-y divide-base-neutral-grey-30 border-base-neutral-grey-20 '>
               {children}
             </table>
