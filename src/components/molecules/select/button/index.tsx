93fd780b2491cea3d38d07c59336c0a7fb6f9124
jerlyrosa <jerlymigueldelarosah@gmail.com>
2023-11-13 15:54:43 -0400

    style: improve select button styles and add disabled option

--- src/components/molecules/select/button/index.tsx
+++ src/components/molecules/select/button/index.tsx
@@ -1,21 +1,28 @@
-import { Listbox } from '@headlessui/react';
-import React from 'react';
-import classNames from 'classnames';
-import ChevronDown from '../../../atoms/icons/chevron-down';
+import { Listbox } from "@headlessui/react";
+import React from "react";
+import classNames from "classnames";
+import ChevronDown from "../../../atoms/icons/chevron-down";
 
 interface Props {
   className?: string;
   Icon?: React.ReactNode;
   error?: boolean;
   placeholder?: string;
+  disabled: boolean;
 }
-export function Button({ className, Icon, error, placeholder }: Props) {
+export function Button({
+  className,
+  Icon,
+  error,
+  placeholder,
+  disabled,
+}: Props) {
   return (
     <Listbox.Button
       className={classNames(
-        'relative flex items-center h-9 w-full rounded-md bg-base-neutral-white disabled:bg-base-neutral-grey-10 p-2 pr-10 text-left text-base-neutral-grey-100 shadow-sm ring-1 ring-inset ring-base-neutral-grey-30 focus:outline-none focus:ring-1 focus:ring-base-primary-100 text-body-md leading-6',
+        "relative flex items-center h-9 w-full rounded-md bg-base-neutral-white disabled:bg-base-neutral-grey-30 p-2 pr-10 text-left text-base-neutral-grey-80 shadow-sm ring-1 ring-inset ring-base-neutral-grey-30 focus:outline-none focus:ring-1 focus:ring-base-primary-100 focus:text-base-neutral-grey-100 text-body-md leading-6",
         {
-          'border border-base-semantic-error-100 focus:ring-transparent': error,
+          "border border-base-semantic-error-100 focus:ring-transparent": error,
         },
         className
       )}
@@ -23,15 +30,22 @@ export function Button({ className, Icon, error, placeholder }: Props) {
       {Icon && Icon}
 
       <span
-        className={classNames('block truncate text-base-neutral-grey-80', {
-          'ml-2': Icon,
+        className={classNames("block truncate ", {
+          "ml-2": Icon,
         })}
       >
         {placeholder}
       </span>
 
-      <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
-        <ChevronDown className='w-5 h-5 text-gray-400' aria-hidden='true' />
+      <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+        <ChevronDown
+          className={classNames(
+            "w-5 h-5",
+            disabled && "text-base-neutral-grey-60"
+          )}
+          fill="currentColor"
+          aria-hidden="true"
+        />
       </span>
     </Listbox.Button>
   );
