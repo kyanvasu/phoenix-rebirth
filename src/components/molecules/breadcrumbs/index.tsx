0e04e9e5348182480b080ea0a634d10c65f46ee1
RivierGrullon <grullonrivier@gmail.com>
2023-12-13 10:50:24 -0400

    feat: add Breadcrumbs component

---
+++ src/components/molecules/breadcrumbs/index.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import React from 'react';
+
+interface Props {
+  pages: {
+    name: string;
+    current: false;
+  }[];
+  textClass?: string;
+}
+
+export default function Breadcrumbs(props: Props) {
+  const { pages, textClass } = props;
+  return (
+    <nav className='flex' aria-label='Breadcrumb'>
+      <ol role='list' className='flex items-center space-x-4'>
+        {pages.map((page) => (
+          <li key={page.name}>
+            <div className='flex items-center'>
+              <svg
+                className='h-5 w-5 flex-shrink-0 text-gray-300'
+                fill='currentColor'
+                viewBox='0 0 20 20'
+                aria-hidden='true'
+              >
+                <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z' />
+              </svg>
+              <div
+                className={classNames('ml-4 text-sm font-medium', textClass, {
+                  'text-gray-500 hover:text-gray-700': !textClass,
+                })}
+                aria-current={page.current ? 'page' : undefined}
+              >
+                {page.name}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
