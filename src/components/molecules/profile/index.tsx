346ade13b6bfad1a1484162b6e590b4bd3cb939d
RivierGrullon <grullonrivier@gmail.com>
2023-12-13 15:51:29 -0400

    fix: text color profile

--- src/components/molecules/profile/index.tsx
+++ src/components/molecules/profile/index.tsx
@@ -8,6 +8,7 @@ interface Props extends React.HTMLProps<HTMLDivElement> {
   src?: string;
   name?: string;
   company?: string;
+  textColor?: string;
 }
 export default function Profile(props: Props) {
   const { src, className, name, company, Size = 'xx-small', ...rest } = props;
@@ -21,10 +22,11 @@ export default function Profile(props: Props) {
 
       <div
         className={classNames(
-          'flex flex-col justify-center text-base-neutral-grey-100',
+          'flex flex-col justify-center',
           {
             'text-body-base': Size === ('medium' || 'small'),
             'text-body-md': Size === 'xx-small',
+            'text-base-neutral-grey-100': !rest.textColor
           }
         )}
       >
