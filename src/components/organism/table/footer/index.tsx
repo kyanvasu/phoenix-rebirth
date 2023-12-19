cc99a2b9c9c81309c251d9961ac68c4dd6cf344f
Rivier Grullon <45011060+RivierGrullon@users.noreply.github.com>
2023-12-19 11:18:00 -0400

    Revert "style: refactor table component styles"

--- src/components/organism/table/footer/index.tsx
+++ src/components/organism/table/footer/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Body } from '../../../atoms';
-import classNames from 'classnames';
 
-interface Props {
-  data: number;
-  className?: string;
-}
-export function TableFooter({ data, className }: Props) {
+export function TableFooter({ data }: { data: number }) {
   return (
-    <tfoot
-      className={classNames('h-[60px', className, {
-        'text-base-neutral-grey-100': !className,
-      })}
-    >
+    <tfoot className='h-[60px] text-base-neutral-grey-100'>
       <tr>
         <td colSpan={3} className='pl-3'>
           <Body.Two>`Showing 1 to 10 of {data} results`</Body.Two>
