586fc6912791832131413a74ba6a09ce39a2f15c
RivierGrullon <grullonrivier@gmail.com>
2023-12-19 11:13:59 -0400

    style: refactor table component styles

--- src/components/organism/table/footer/index.tsx
+++ src/components/organism/table/footer/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Body } from '../../../atoms';
+import classNames from 'classnames';
 
-export function TableFooter({ data }: { data: number }) {
+interface Props {
+  data: number;
+  className?: string;
+}
+export function TableFooter({ data, className }: Props) {
   return (
-    <tfoot className='h-[60px] text-base-neutral-grey-100'>
+    <tfoot
+      className={classNames('h-[60px', className, {
+        'text-base-neutral-grey-100': !className,
+      })}
+    >
       <tr>
         <td colSpan={3} className='pl-3'>
           <Body.Two>`Showing 1 to 10 of {data} results`</Body.Two>
