8f7c8e23dd68b80cb903598674f645e51658a791
jerlyrosa <jerlymigueldelarosah@gmail.com>
2024-01-04 12:45:42 -0400

    feat: Add custom styles configuration for empty state title

--- next/src/app/client.tsx
+++ next/src/app/client.tsx
@@ -1,14 +1,22 @@
-'use client';
+"use client";
 
-import { ClientCoreStore } from '@kanvas/phoenix/client';
-import { PropsWithChildren } from 'react';
-import { sdk } from './layout';
-import { Provider } from 'jotai';
+import { ClientCoreStore } from "@kanvas/phoenix/client";
+import { PropsWithChildren } from "react";
+import { sdk } from "./layout";
+import { Provider } from "jotai";
+import { DefaultStylesConfig } from "@kanvas/phoenix";
+import { StylesConfigTypes } from "@kanvas/phoenix/model/types/default-styles-config";
 
 export default function RootClientLayout({ children }: PropsWithChildren) {
+  const customStylesConfig: StylesConfigTypes = {
+    emptyState: {
+      ...DefaultStylesConfig.emptyState,
+      title: "font-semibold text-base-neutral-grey-50",
+    },
+  };
   return (
     <Provider>
-      <ClientCoreStore sdk={sdk}>
+      <ClientCoreStore sdk={sdk} stylesConfig={customStylesConfig}>
         <>{children}</>
       </ClientCoreStore>
     </Provider>
