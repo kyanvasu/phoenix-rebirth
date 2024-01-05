cb6a5e978a0d88e32516b19cd3907cc527258ddc
Lizandro <lizandro@mctekk.com>
2024-01-05 00:21:54 -0400

    feat: add text input theme

--- src/components/molecules/form/text-input/index.tsx
+++ src/components/molecules/form/text-input/index.tsx
@@ -1,9 +1,11 @@
 import classNames from 'classnames';
 import React from 'react';
 import { HelpText, Label } from '../../../atoms';
+import { InputStyles } from '../../../../model/types';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  theme?: InputStyles;
   helpText?: string;
   error?: boolean;
 }
@@ -11,6 +13,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 export function TextInput({
   className,
   label,
+  theme,
   required,
   helpText,
   error,
@@ -19,25 +22,25 @@ export function TextInput({
   return (
     <div
       className={classNames(
-        'flex flex-col gap-[6px] font-normal text-caption-md',
+        theme?.container,
         {
           'text-base-neutral-grey-80': !className,
         },
         className
       )}
     >
-      {label && <Label required={required}>{label}</Label>}
+      {label && (
+        <Label className={theme?.label} required={required}>
+          {label}
+        </Label>
+      )}
       <div className='relative shadow-sm'>
         <input
-          className={classNames(
-            'block w-full h-9 rounded-md border text-body-md disabled:bg-base-neutral-grey-30',
-            className,
-            {
-              'border-red-500': error,
-              'border-base-neutral-grey-30 p-2 text-base-neutral-grey-100 placeholder:text-base-neutral-grey-70 focus:outline-none focus:border-base-primary-80':
-                !className,
-            }
-          )}
+          className={classNames(theme?.input, className, {
+            'border-red-500': error,
+            'border-base-neutral-grey-30 p-2 text-base-neutral-grey-100 placeholder:text-base-neutral-grey-70 focus:outline-none focus:border-base-primary-80':
+              !className,
+          })}
           {...others}
         />
       </div>
