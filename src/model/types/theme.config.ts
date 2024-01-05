9b0101d07c3302ce2eed431e20e24a9de127b9fe
Lizandro <lizandro@mctekk.com>
2024-01-05 00:22:37 -0400

    feat: add login theme types

--- src/model/types/theme.config.ts
+++ src/model/types/theme.config.ts
@@ -4,6 +4,36 @@ export type EmptyStateStyles = {
   subtitle: string;
 };
 
+export type InputStyles = {
+  container: string;
+  label: string;
+  input: string;
+  error: string;
+};
+
+
+export type ActionStyles = {
+  container: string;
+  span: string;
+  button: string;
+  link: string;
+  titleThree: string;
+  checkBoxTheme: InputStyles;
+};
+
+export type LoginFormStyles = {
+  container: string;
+  actionTheme: ActionStyles;
+  textInputTheme: InputStyles;
+};
+
+export type LoginPageStyles = {
+  formTheme: LoginFormStyles;
+  title: string;
+  container: string;
+};
+
 export type ThemeTypes = {
   emptyState: EmptyStateStyles;
+  login: LoginPageStyles;
 };
