75f5f5aa5adb068982a3f2bdb91e9df386c055f1
jerlyrosa <jerlymigueldelarosah@gmail.com>
2024-01-05 11:41:36 -0400

    chore(theme): update base theme config

--- src/model/types/theme.config.ts
+++ src/model/types/theme.config.ts
@@ -4,6 +4,19 @@ export type EmptyStateStyles = {
   subtitle: string;
 };
 
+export type DropdownTypes = {
+  container: string;
+  button: string;
+  panel: string;
+  list: string;
+  item: {
+    container: string;
+    text: string;
+    icon: string;
+    hover: string;
+  };
+};
 export type ThemeTypes = {
   emptyState: EmptyStateStyles;
+  dropdown: DropdownTypes;
 };
