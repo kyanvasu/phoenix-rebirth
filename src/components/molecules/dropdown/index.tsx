59c0b0da0b702df1d0b2cdb5586e8dd774c93cea
jerlyrosa <jerlymigueldelarosah@gmail.com>
2024-01-05 11:42:42 -0400

    style: remove unnecessary code and update dropdown styles

--- src/components/molecules/dropdown/index.tsx
+++ src/components/molecules/dropdown/index.tsx
@@ -2,33 +2,28 @@ import { Popover } from "@headlessui/react";
 import React, { PropsWithChildren } from "react";
 import { DropdwonItemInterface } from "../../../model/types";
 import classNames from "classnames";
+import { useClientContext } from "../../../client";
 
 interface Props extends PropsWithChildren {
   items: DropdwonItemInterface[];
   onChange?: (value: DropdwonItemInterface) => void;
   className?: string;
-};
+}
 
 export default function Dropdown(props: Props) {
+  const { theme } = useClientContext();
   const { children, className, items = [], onChange } = props;
-
-  const classname = classNames(
-    'absolute  rounded-sm border  shadow-elevation-3',
-    className,
-    className, {
-    'bg-base-neutral-white border-base-primary-30 text-base-neutral-grey-60 stroke-base-neutral-grey-60 hover:text-base-primary-60 hover:stroke-base-primary-60': !className,
-  }
-  );
-
   return (
-    <Popover className="relative select-none w-min">
-      <Popover.Button className="outline-none">
+    <Popover className={theme.dropdown.container}>
+      <Popover.Button className={theme.dropdown.button}>
         {children}
       </Popover.Button>
 
-      <Popover.Panel className={classname}>
-        <ul className="list-none p-0 m-0">
-          {items.map((value) => <DropdownItem {...value} onClick={() => onChange?.(value)} />)}
+      <Popover.Panel className={classNames(theme.dropdown.panel, className)}>
+        <ul className={theme.dropdown.list}>
+          {items.map((value) => (
+            <DropdownItem {...value} onClick={() => onChange?.(value)} />
+          ))}
         </ul>
       </Popover.Panel>
     </Popover>
@@ -42,17 +37,19 @@ interface ItemProps extends DropdwonItemInterface {
 function DropdownItem(props: ItemProps) {
   const { text, Icon, onClick } = props;
 
-  const classname = classNames(
-    'flex gap-2 py-1.5 px-[18px] min-w-[167px] cursor-pointer items-center',
-    'text-base-neutral-grey-60 stroke-base-neutral-grey-60 hover:text-base-primary-60 hover:stroke-base-primary-60',
+  const { theme } = useClientContext();
+
+  const itemClassname = classNames(
+    theme.dropdown.item.container,
+    theme.dropdown.item.hover
   );
 
   const handleClick = () => onClick?.();
 
   return (
-    <li onClick={handleClick} className={classname}>
+    <li onClick={handleClick} className={itemClassname}>
       {Icon && Icon}
-      <span className="text-body-md text-inherit">{text}</span>
+      <span className={theme.dropdown.item.text}>{text}</span>
     </li>
   );
 }
