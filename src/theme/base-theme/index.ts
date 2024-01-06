3bdd14817f500e086ae7c5964081f217228b83e2
Lizandro <lizandro@mctekk.com>
2024-01-05 21:55:43 -0400

    refactor: base config theme for login page

--- src/theme/base-theme/index.ts
+++ src/theme/base-theme/index.ts
@@ -8,28 +8,26 @@ export const BaseTheme: ThemeTypes = {
   },
   login: {
     formTheme: {
-      container: 'flex flex-col gap-y-[38px]',
-      actionTheme: {
-        container: '',
-        span: 'flex flex-row gap-x-4',
-        button: 'justify-center w-full my-6',
-        link: 'font-semibold text-base-primary-100 text-body-md',
-        titleThree: 'text-base-neutral-grey-80',
-        checkBoxTheme: {
-          container: 'font-normal text-body-md text-base-neutral-grey-100 flex items-center w-fit gap-2',
-          label: 'font-semibold text-base-neutral-grey-100',
-          input: 'border border-base-neutral-grey-40 rounded-md h-10 px-4',
-          error: 'text-base-red-100 text-body-sm',
-        },
-      },
-      textInputTheme: {
-        container: ' flex flex-col gap-[6px] font-normal text-caption-md',
-        label: 'font-semibold text-base-neutral-grey-100',
-        input: 'block w-full h-9 rounded-md border text-body-md disabled:bg-base-neutral-grey-30',
-        error: 'text-base-red-100 text-body-sm',
-      },
+      container: '',
+      span: 'flex flex-row gap-x-4',
+      button: 'justify-center w-full my-6 ',
+      link: 'font-semibold text-base-primary-100 text-body-md',
+      title: 'text-base-neutral-grey-80',
     },
-    container: '',
+    checkBoxTheme: {
+      container: 'flex justify-between items-center',
+      label: 'font-semibold text-base-neutral-grey-100',
+      input: 'border border-base-neutral-grey-40 rounded-md h-10 px-4',
+      error: 'text-base-red-100 text-body-sm',
+    },
+    textInputTheme: {
+      container: ' flex flex-col gap-[6px] font-normal text-caption-md',
+      label: 'font-semibold text-base-neutral-grey-100',
+      input:
+        'block w-full h-9 rounded-md border text-body-md disabled:bg-base-neutral-grey-30',
+      error: 'border-red-500 text-base-red-100 text-body-sm',
+    },
+    container: 'flex flex-col gap-y-[38px]',
     title: 'mt-6 mb-10 font-bold text-base-neutral-grey-100',
   },
 };
