bd2a08cafdfa7181f74d04d689a3d0a2f8dfad6a
buttercubz <ericksosagarcias@gmail.com>
2025-08-28 19:05:41 -0400

    chore: update components

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { AspectRatio as AspectRatioPrimitive } from "radix-ui";
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
 
-const AspectRatio = AspectRatioPrimitive.Root;
+function AspectRatio({
+  ...props
+}: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
+  return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />;
+}
 
 export { AspectRatio };
